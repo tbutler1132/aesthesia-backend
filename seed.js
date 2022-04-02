d715aa38cc120d80b1d60bce955db0e86f619ed6
Timothy Butler <tbutler1132@gmail.com>
2022-04-02 10:32:44 -0400

    World seeds

--- seed.js
+++ seed.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import dotenv from 'dotenv'
 import User from './models/User.js'
+import World from './models/World.js'
 dotenv.config()
 
 const mdb = process.env.MONGO_DB_URI
@@ -31,9 +32,20 @@ const users = [
     },
 ]
 
+const worlds = [
+    {
+        description: "Ipsolum",
+        referenceSongs: ["Gold Digger", "Stronger", "Runaway"],
+        referenceImages: ["Blue", "Green"],
+        tags: ["Dark", "Cool"]
+    }
+]
+
 const seedDB = async () => {
     await User.deleteMany({})
     await User.insertMany(users)
+    await World.deleteMany({})
+    await World.insertMany(worlds)
 }
 
 seedDB().then(() => {
