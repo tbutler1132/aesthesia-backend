5d8dab6a95af72540cb0e8f63e1c53f69af9cb39
Timothy Butler <tbutler1132@gmail.com>
2022-04-18 10:32:58 -0400

    Upload to Cloudinary Working

--- controllers/submission.js
+++ controllers/submission.js
@@ -22,50 +22,42 @@ export const updateSubmission = async (req, res) => {
 export const createSubmission = async (req, res) => {
 
     const { songId } = req.query
-    const { files } = req.body
+    const files = req.body
 
     try {
-        const uploadedResponse = await cloudinary.uploader.upload(files, "dev_setups", {
-            resource_type: "video"
-        })
-        const { secure_url } = uploadedResponse
-    } catch (error) {
-        
+        const stems = []
+    
+        for(const stem of files.stems){
+            const uploadedResponse = await cloudinary.uploader.upload(files.stems[0].file.files, "dev_setups", {
+                resource_type: "video"
+            })
+            const { secure_url } = uploadedResponse 
+            stems.push({
+                file: secure_url,
+                track: stem.track
+            })
+        }
 
+        const submission = new Submission({
+            bpm: files.bpm,
+            description: files.description,
+            scale: files.scale,
+            stems
+        })
+
+        const newSubmission = await submission.save()
+
+        const song = await Song.findById(songId)
+
+        song.currentIteration.submissions.push(newSubmission._id)
+
+        await song.save()
+
+        res.status(200).json(submission)
+    } catch (error) {
+        console.log(error)
+
+        res.status(500).json('Error', error)  
     }
 
-
-
-
-    // const { 
-    //     bpm,
-    //     description,
-    //     scale,
-    //     stems
-    //  } = req.body
-
-    // const submission = new Submission({
-    //     bpm,
-    //     description,
-    //     scale,
-    //     stems
-    // })
-    
-    // try {
-
-    //     const newSubmission = await submission.save()
-
-    //     const song = await Song.findById(songId)
-
-    //     song.currentIteration.submissions.push(newSubmission._id)
-
-    //     await song.save()
-
-    //     res.status(200).json(submission)
-        
-    // } catch (error) {
-    //     console.log(error)
-
-    //     res.status(500).json('Error', error)    
-    // }
 }
